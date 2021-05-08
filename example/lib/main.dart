7f61b8a9a91e9f9796665d734c9fd27efcba1dbc
GoyalSidhant <sidhant702@gmail.com>
2021-05-08 15:53:31 +0530

    Example App tested

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:bubble/bubble.dart';
 import 'package:dialogflow_flutter/googleAuth.dart';
+import 'package:dialogflow_flutter_example/chatscreen.dart';
 import 'package:flutter/material.dart';
 import 'dart:async';
 
@@ -17,8 +18,6 @@ class MyApp extends StatefulWidget {
 
 class _MyAppState extends State<MyApp> {
   String _platformVersion = 'Unknown';
-  final messageInsert = TextEditingController();
-  List<Map> messsages = List();
 
   @override
   void initState() {
@@ -41,125 +40,15 @@ class _MyAppState extends State<MyApp> {
     });
   }
 
-  void response(query) async {
-    AuthGoogle authGoogle =
-        await AuthGoogle(fileJson: "assets/farmtech-fh9j-3db6e0409c71.json")
-            .build();
-    DialogFlow dialogflow = DialogFlow(authGoogle: authGoogle, language: "en");
-    AIResponse aiResponse = await dialogflow.detectIntent(query);
-    setState(() {
-      messsages.insert(0, {
-        "data": 0,
-        "message": aiResponse.getListMessage()[0]["text"]["text"][0].toString()
-      });
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          centerTitle: true,
-          toolbarHeight: 70,
-          backgroundColor: Colors.white,
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.only(
-              bottomLeft: Radius.circular(30),
-            ),
-          ),
-          elevation: 10,
-          title: Text("Dailog Flow Chatbot"),
-        ),
-        body: Container(
-          child: Column(
-            children: <Widget>[
-              Flexible(
-                  child: ListView.builder(
-                      reverse: true,
-                      itemCount: messsages.length,
-                      itemBuilder: (context, index) => chat(
-                          messsages[index]["message"].toString(),
-                          messsages[index]["data"]))),
-              Divider(
-                height: 6.0,
-              ),
-              Container(
-                padding: EdgeInsets.only(left: 15.0, right: 15.0, bottom: 20),
-                margin: const EdgeInsets.symmetric(horizontal: 8.0),
-                child: Row(
-                  children: <Widget>[
-                    Flexible(
-                        child: TextField(
-                      controller: messageInsert,
-                      decoration: InputDecoration.collapsed(
-                          hintText: "Send your message",
-                          hintStyle: TextStyle(
-                              fontWeight: FontWeight.bold, fontSize: 18.0)),
-                    )),
-                    Container(
-                      margin: EdgeInsets.symmetric(horizontal: 4.0),
-                      child: IconButton(
-                          icon: Icon(
-                            Icons.send,
-                            size: 30.0,
-                          ),
-                          onPressed: () {
-                            if (messageInsert.text.isEmpty) {
-                              print("empty message");
-                            } else {
-                              setState(() {
-                                messsages.insert(0,
-                                    {"data": 1, "message": messageInsert.text});
-                              });
-                              response(messageInsert.text);
-                              messageInsert.clear();
-                            }
-                          }),
-                    )
-                  ],
-                ),
-              ),
-              SizedBox(
-                height: 15.0,
-              )
-            ],
-          ),
-        ),
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primaryColor: Colors.blue,
+        primarySwatch: Colors.blue,
       ),
-    );
-  }
-
-  Widget chat(String message, int data) {
-    return Padding(
-      padding: EdgeInsets.all(10.0),
-      child: Bubble(
-          radius: Radius.circular(15.0),
-          color: data == 0 ? Colors.blue : Colors.orangeAccent,
-          elevation: 0.0,
-          alignment: data == 0 ? Alignment.topLeft : Alignment.topRight,
-          nip: data == 0 ? BubbleNip.leftBottom : BubbleNip.rightTop,
-          child: Padding(
-            padding: EdgeInsets.all(2.0),
-            child: Row(
-              mainAxisSize: MainAxisSize.min,
-              children: <Widget>[
-                CircleAvatar(
-                  backgroundImage: AssetImage(
-                      data == 0 ? "assets/bot.png" : "assets/user.png"),
-                ),
-                SizedBox(
-                  width: 10.0,
-                ),
-                Flexible(
-                    child: Text(
-                  message,
-                  style: TextStyle(
-                      color: Colors.white, fontWeight: FontWeight.bold),
-                ))
-              ],
-            ),
-          )),
+      home: ChatBotScree(),
     );
   }
 }
