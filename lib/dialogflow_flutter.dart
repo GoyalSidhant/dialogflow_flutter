b8edec025b407db6c9378ae276080e073bd0c898
GoyalSidhant <sidhant702@gmail.com>
2021-05-09 12:38:05 +0530

    Web support

--- lib/dialogflow_flutter.dart
+++ lib/dialogflow_flutter.dart
@@ -2,7 +2,6 @@ import 'dart:async';
 
 import 'package:dialogflow_flutter/googleAuth.dart';
 import 'package:flutter/services.dart';
-
 import 'dart:convert';
 import 'dart:io';
 import 'package:meta/meta.dart';
@@ -113,13 +112,14 @@ class AIResponse {
 }
 
 class DialogFlow {
-  static const MethodChannel _channel =
-      const MethodChannel('dialogflow_flutter');
-
-  static Future<String> get platformVersion async {
-    final String version = await _channel.invokeMethod('getPlatformVersion');
-    return version;
-  }
+  /* static void registerWith(Registrar registrar) {
+    final MethodChannel channel = MethodChannel(
+        'plugins.flutter.io/url_launcher',
+        const StandardMethodCodec(),
+        registrar.messenger);
+    final DialogFlow instance = DialogFlow();
+    channel.setMethodCallHandler();
+  } */
 
   final AuthGoogle authGoogle;
   final String language;
--- test/dialogflow_flutter_test.dart
+++ test/dialogflow_flutter_test.dart
@@ -17,7 +17,4 @@ void main() {
     channel.setMockMethodCallHandler(null);
   });
 
-  test('getPlatformVersion', () async {
-    expect(await DialogFlow.platformVersion, '42');
-  });
 }
